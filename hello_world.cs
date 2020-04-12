202a1b0a6f58220f4d4ea64442727ab9d539f8bd
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-04-11 21:42:52 -0400

    First Commit

---
+++ hello_world.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Hello World!");
+     }
+  }
+}
+
+// Console.WriteLine() is a command that prints text to a console. Whatever is in between the parentheses will be printed to the console.
